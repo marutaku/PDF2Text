9fba37b2599bc0cea815c8421053b74dac4b0661
丸山拓己 <satouredstone@gmail.com>
2018-02-14 01:00:55 +0900

    Initialize commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+import sys
+
+pdf_path = sys.argv[1]
+
+
+
+rsrcmgr = PDFResourceManager()
+rettxt = StringIO()
+laparams = LAParams()
+
+device = TextConverter(rsrcmgr, rettxt, codec='utf-8', laparams=laparams)
+with open(pdf_path, 'rb') as fp:
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.get_pages(fp, pagenos=None, maxpages=0, password=None, caching=True, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = rettxt.getvalue()
+    text = text.split('Abstract')[1]
+    text = text.replace('\n', ' ')
+    text = text.replace('  ', '\n')
+
+    device.close()
+    rettxt.close()
+# 彼女氏のいたずらコード
+miki = text
+new_file_name = pdf_path + '.txt'
+with  open(new_file_name, 'w') as f:
+    f.write(miki)
+
